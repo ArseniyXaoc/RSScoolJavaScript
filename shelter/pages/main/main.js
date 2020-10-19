dff525facf3bed71b6d5eb003ae6cdf54aab01da
ArseniyXaoc <xaoc190789@gmail.com>
2020-10-19 11:54:25 +0300

    fix; burger menu

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -1,3 +1,17 @@
 // неактивные ссылки
 document.querySelector(".navigation__link:nth-child(3) > a").setAttribute('onclick', 'return false');
 document.querySelector(".navigation__link:nth-child(4) > a").setAttribute('onclick', 'return false');
+document.querySelector(".logo__title").setAttribute('onclick', 'return false');
+
+//JSON load Pets.json
+let requestURL = 'http://localhost:5500/shelter/pages/main/pets.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+    let Pets = request.response;    
+    console.log(Pets);
+}
--- shelter/pages/pets/pets.js
+++ shelter/pages/pets/pets.js
@@ -1,2 +1,7 @@
 
 //Hamb
+let checkbox = document.querySelector('#menu__toggle');
+let overlay = document.querySelector("body > div > header > header > div.hamburger-menu > div");
+console.log(checkbox);
+
+overlay.addEventListener('click', () => checkbox.checked = false);

5402adae1019dec2778493f592e6b307829ba8d7
Бурячек Арсений Павлович <@castle.in>
2020-10-26 15:39:04 +0300

    fix

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -1,6 +1,6 @@
 // неактивные ссылки
-document.querySelector(".navigation__link:nth-child(3) > a").setAttribute('onclick', 'return false');
-document.querySelector(".navigation__link:nth-child(4) > a").setAttribute('onclick', 'return false');
+//document.querySelector(".navigation__link:nth-child(3) > a").setAttribute('onclick', 'return false');
+//document.querySelector(".navigation__link:nth-child(4) > a").setAttribute('onclick', 'return false');
 document.querySelector(".logo__title").setAttribute('onclick', 'return false');
 
 let checkbox = document.querySelector('#menu__toggle');

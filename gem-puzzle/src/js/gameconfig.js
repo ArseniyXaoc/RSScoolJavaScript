278ffb48a753ebf78d30c22fd2b3817f5a2de964
ArseniyXaoc <xaoc190789@gmail.com>
2020-11-25 15:58:45 +0300

    fix: add gameconfig 'magik'

---
+++ gem-puzzle/src/js/gameconfig.js
@@ -0,0 +1,34 @@
+const gameconfig = {
+    'size-0': {
+      numOfPuzzle: 8,
+      elementSize: 33.33,
+      puzzleLength: 3,
+    },
+    'size-1': {
+      numOfPuzzle: 15,
+      elementSize: 25,
+      puzzleLength: 4,
+    },
+    'size-2': {
+      numOfPuzzle: 24,
+      elementSize: 20,
+      puzzleLength: 5,
+    },
+    'size-3': {
+      numOfPuzzle: 35,
+      elementSize: 16.66,
+      puzzleLength: 6,
+    },
+    'size-4': {
+      numOfPuzzle: 48,
+      elementSize: 14.286,
+      puzzleLength: 7,
+    },
+    'size-5': {
+      numOfPuzzle: 63,
+      elementSize: 12.5,
+      puzzleLength: 8,
+    },
+  }
+
+  export {gameconfig};
--- gem-puzzle/src/js/index.js
+++ gem-puzzle/src/js/index.js
@@ -14,6 +14,10 @@ import {
   timerX,
 } from './time';
 
+import {
+  gameconfig
+} from './gameconfig';
+
 time();
 
 class Settings {
@@ -77,40 +81,7 @@ settings.restart.addEventListener('click', () => {
   if (field) {
     document.body.removeChild(field);
     document.body.removeChild(document.querySelector('.move'));
-  }
-
-  const gameconfig = {
-    'size-0': {
-      numOfPuzzle: 8,
-      elementSize: 33.33,
-      puzzleLength: 3,
-    },
-    'size-1': {
-      numOfPuzzle: 15,
-      elementSize: 25,
-      puzzleLength: 4,
-    },
-    'size-2': {
-      numOfPuzzle: 24,
-      elementSize: 20,
-      puzzleLength: 5,
-    },
-    'size-3': {
-      numOfPuzzle: 35,
-      elementSize: 16.66,
-      puzzleLength: 6,
-    },
-    'size-4': {
-      numOfPuzzle: 48,
-      elementSize: 14.286,
-      puzzleLength: 7,
-    },
-    'size-5': {
-      numOfPuzzle: 63,
-      elementSize: 12.5,
-      puzzleLength: 8,
-    },
-  }
+  }  
 
   clearInterval(timer);
   createPuzzleField = new Puzzle;
